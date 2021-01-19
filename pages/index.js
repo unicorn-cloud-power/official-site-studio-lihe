4c87762db9061c044c8fc3fa39da943b668a8779
Kuma Li <li.jun.kuma@gmail.com>
2021-01-20 00:00:39 +0800

    add bg and cloud animation, logo, favicon

--- pages/index.js
+++ pages/index.js
@@ -1,64 +1,49 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>独角云动力 - 首页</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel='stylesheet' href='https://cdn.bootcdn.net/ajax/libs/font-awesome/5.15.2/css/regular.min.css' />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <nav className="nav" id="menu">
+        <div className="wrap">
+          <div className="brand">
+            <img src="/logo.svg" />
+            <span>独角云动力</span>
+          </div>
+          <button id="mobile-btn" className="hamburger-btn">
+            <span className="hamburger-line"></span>
+            <span className="hamburger-line"></span>
+            <span className="hamburger-line"></span>
+          </button>
+          <ul className="top-menu" id="top-menu">
+            <li><a href="#">产品服务</a></li>
+            <li><a href="#">联系我们</a></li>
+          </ul>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+      </nav>
+      <header className="hero">
+        <div className="content">
+          <p>独角云动力</p>
+          <p>专业小程序定制</p>
+          <div className="clouds">
+            <div className="cloud x1"></div>
+            <div className="cloud x2"></div>
+            <div className="cloud x3"></div>
+            <div className="cloud x4"></div>
+            <div className="cloud x5"></div>
+          </div>
+        </div>
+      </header>
+      <footer className="footer">
+        <div className="tab-row">
+          <div className="col-6">&copy; 2021 上海里禾网络科技工作室 </div>
+          <div className="col-6"> <a href="http://beian.miit.gov.cn" target="_blank">沪ICP备20003440号-1</a></div>
+        </div>
       </footer>
     </div>
   )
